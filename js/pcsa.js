a676bd31ae7ff1276cb1ae8ec7d75ee35a12e7c3
root <root@hanzou.awakimjan.nl>
2017-02-11 20:58:30 +0100

    New theme with bootstrap as basis. Front page partly finished, all files copied from Pingen version.

--- js/pcsa.js
+++ js/pcsa.js
@@ -1,63 +1,68 @@
-var $ = jQuery;
+//Google maps stuff
+function initialize() {
 
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll > 0) {
-        $("nav").addClass("scrolling");
-        $("nav li a").addClass("scrolling");
-        $("#navigation").addClass("scrolling");
-        $("h1").addClass("scrolling");
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    zoom: 1,
+    center: new google.maps.LatLng(52.225089, 6.887767),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }),
+
+  myMapsId = 'zTJMnapVaP1o.km0KB9QZ43hM';
+  new google.maps
+    .KmlLayer({
+      map: map,
+      url: 'https://www.google.com/maps/d/kml?mid=' + myMapsId
+  });
+
+  var styles = [{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"hue":149},{"saturation":-78},{"lightness":0}]},{"featureType":"road.highway","stylers":[{"hue":-31},{"saturation":-20},{"lightness":-20.8}]},{"featureType":"poi","elementType":"label","stylers":[{"visibility":"off"}]},{"featureType":"landscape","stylers":[{"hue":163},{"saturation":-26},{"lightness":20.1}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"hue":2},{"saturation":100},{"lightness":-80.1}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off" }]}];
+  map.setOptions({styles: styles});
+}
+
+
+$(document).ready(function(){    
+
+  // Scrollbar change
+  var scroll_start = $(this).scrollTop();
+  var offset = 300;
+
+  //Change nav on scroll
+  $(document).scroll(function() { 
+    scroll_start = $(this).scrollTop();
+
+    if(scroll_start > offset) {
+      $(".navbar").addClass("navScroll");
+      //$(".secondary").addClass("navScroll");
     } else {
-        $("nav").removeClass("scrolling");
-        $("nav li a").removeClass("scrolling");
-        $("#navigation").removeClass("scrolling");
-        $("h1").removeClass("scrolling");
-    }
-});
-
-$(window).ready(function() {
-    var menuOpen = false;
-
-    function closeMenu() {
-        var headerHeight = $('#navigation').height();
-        $('#mobile-menu .menu').css('bottom', headerHeight + 'px');
-        menuOpen = false;
+      $(".navbar").removeClass("navScroll");
+      //$(".secondary").removeClass("navScroll");
     }
 
-    function openMenu() {
-        var menuHeight = $('#mobile-menu .menu').height();
-        $('#mobile-menu .menu').css('bottom', -menuHeight + 'px');
-        menuOpen = true;
+  });
+
+  //Hide/show login form
+  $("#loginlink").click(function(){
+    $('#login').toggle("fast");
+  });
+
+  $(".navbar-toggle").click(function(){
+    if(!$(this).hasClass("collapsed")){ //If we collapse the menu
+      if($('#login:visible').length == 1){ //And if login is open
+        $('#login').hide(0); //Hide login
+      }
     }
+  });
 
-    $('#menu-button').click(function(e) {
-        e.stopPropagation();
-        if (menuOpen) {
-            closeMenu();
-        } else {
-            openMenu();
-        }
-    });
+  //Adjust height if needed
+  $( "#main .container" ).each(function() {
+    $this = $( this );
+      if($("#heerinfo").height() > $( this ).height()){
+        $this.height( $("#heerinfo").height() + 200);
+      }
+  });
 
-    $('.menu').bind('touchstart', function(e) {
-        e.stopPropagation();
-    });
+  //Only load map if on Reizen page
+  if ($("#map-canvas").length > 0) {
+    google.maps.event.addDomListener(window, 'load', initialize);
+  }
 
-    $('#navbar').bind('touchstart', function(e) {
-        e.stopPropagation();
-    });
-
-    $(window).bind('touchstart', function() {
-        if (menuOpen) closeMenu();
-    });
-
-    // $(window).scroll(function() {
-    //     if (menuOpen) closeMenu();
-    // });
-
-    $(window).resize(function() {
-        if (menuOpen) closeMenu();
-    });
-
-    closeMenu();
 });
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,109 +1,148 @@
 <?php
+
 /**
-* @file
-* Returns the HTML for a single Drupal page.
-*
-* Complete documentation for this file is available online.
-* @see https://drupal.org/node/1728148
-*/
+ * @file
+ * Default theme implementation to display a single Drupal page.
+ *
+ * The doctype, html, head and body tags are not in this template. Instead they
+ * can be found in the html.tpl.php template in this directory.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *   least, this will always default to /.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/bartik.
+ * - $is_front: TRUE if the current page is the front page.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *   when linking to the front page. This includes the language domain or
+ *   prefix.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $site_name: The name of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *   in theme settings.
+ *
+ * Navigation:
+ * - $main_menu (array): An array containing the Main menu links for the
+ *   site, if they have been configured.
+ * - $secondary_menu (array): An array containing the Secondary menu links for
+ *   the site, if they have been configured.
+ * - $breadcrumb: The breadcrumb trail for the current page.
+ *
+ * Page content (in order of occurrence in the default page.tpl.php):
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title: The page title, for use in the actual HTML content.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ * - $messages: HTML for status and error messages. Should be displayed
+ *   prominently.
+ * - $tabs (array): Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node).
+ * - $action_links (array): Actions local to the page, such as 'Add menu' on the
+ *   menu administration interface.
+ * - $feed_icons: A string of all feed icons for the current page.
+ * - $node: The node object, if there is an automatically-loaded node
+ *   associated with the page, and the node ID is the second argument
+ *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - $page['help']: Dynamic help text, mostly for admin pages.
+ * - $page['highlighted']: Items for the highlighted content region.
+ * - $page['content']: The main content of the current page.
+ * - $page['sidebar_first']: Items for the first sidebar.
+ * - $page['sidebar_second']: Items for the second sidebar.
+ * - $page['header']: Items for the header region.
+ * - $page['footer']: Items for the footer region.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see template_process()
+ * @see html.tpl.php
+ *
+ * @ingroup themeable
+ */
 ?>
 
-<nav id="navigation" class="container<?php echo drupal_is_front_page() ? ' frontpage' : ''; ?> <?php echo user_is_anonymous() ? 'anonymous' : 'authenticated' ?>">
-    <div id="navbar">
-        <a href="/">
-            <h1><?php print $site_name; ?></h1>
-        </a>
-        <div id='menu-button'></div>
-    </div>
-    <div id='desktop-menu'>
-        <?php if (user_is_anonymous()) : ?>
-            <div id='anonymous-menu'>
-                <?php print render($page['desktop_menu_anonymous']); ?>
-            </div>
-        <?php else: ?>
-            <div id='primary-menu'>
-                <?php print render($page['desktop_menu_primary']); ?>
-            </div>
-            <div id='secondary-menu'>
-                <?php print render($page['desktop_menu_secondary']); ?>
-            </div>
-        <?php endif ?>
-    </div>
-    <div id='mobile-menu'>
-        <?php print render($page['mobile_menu']); ?>
-    </div>
-</nav>
+  <div id="page-wrapper"><div id="page">
+  
+    <?php if ($main_menu || $secondary_menu): ?>
+      <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            </button>
+            <?php if ($site_name): ?>
+              <a class="navbar-brand"><?php print $site_name; ?></a>
+            <?php endif; ?>
+          </div>
 
-<?php if (drupal_is_front_page()) : ?>
-    <div id="intro-wrapper">
-        <section id="intro">
-            <div id="intro-inner">
-                <div id="background-wrapper">
-                    <div id="background-overlay"></div>
-                    <?php srand(date('Ymdh')); // New image every hour ?>
-                    <img id="background-image" src="<?php echo path_to_theme() ?>/images/promo/promo_<?php echo rand(1,6); ?>.jpg" />
-                </div>
-                <img id="wapen"/src="<?php echo path_to_theme() ?>/images/wapen-nieuw.png"/>
-                <h2>Heerendispuut P.C.S.A. Incognito</h2>
-            </div>
-        </section>
-    </div>
-<?php endif ?>
+          <!-- display the navigation items, load them from the selected main_menu in drupal -->
+          <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+            <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('class' => array('nav navbar-nav')))); ?>
+          </div>
 
-<div id="page">
-
-    <?php if (drupal_is_front_page()) : ?>
-
-        <div class="content-wrapper">
-        <?php if (user_is_anonymous()) :?>
-            <section id="welkom">
-                <div class="container">
-                    <h3>Welkom</h3>
-                    <span>
-                        Welkom op de website van Heerendispuut P.C.S.A. Incognito: het eerste, oudste, voornaemste en tevens rijckste dispuut van Europese studentenvereniging AEGEE-Enschede. Primum Collegium Societatis AEGEE-Enschede Incognito is op 26 augustus 1991 opgericht vanuit de gedachte en het streven om de kennis en ervaring verzameld binnen AEGEE niet verloren te laten gaan, maar om deze vast te houden en door te geven middels een dispuut.
-                    </span>
-                    <br /><a href="/geschiedenis" class="button">Geschiedenis</a>
-                </div>
-            </section>
-            <section id="nieuws" class="content">
-                <?php print render($page['frontpage_news']); ?>
-            </section>
-        <?php else : ?>
-            <section id="new_activity" class="content">
-                <?php print render($page['frontpage_updates']); ?>
-            </section>
-        <?php endif ?>
         </div>
+      </nav>
+    <?php endif; ?> <!-- $main_menu, $secondary_menu -->
 
-    <?php else : ?>
-    <div class="content-wrapper" class="column" role="main">
-        <?php print render($page['highlighted']); ?>
-        <?php print render($title_prefix); ?>
-        <?php if ($title): ?>
-            <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-        <?php endif; ?>
-        <?php print render($title_suffix); ?>
-        <?php print $messages; ?>
-        <?php print render($tabs); ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-        <?php endif; ?>
-        <section class="content">
-            <?php print render($page['content']); ?>
+    <?php if (drupal_is_front_page()): ?>
+        <section id="header">
+          <div class="container">
+            <img src="<?php echo path_to_theme() ?>/images/wapen-nieuw.png" alt="Logo" class="img-responsive" />
+            <h1>Heerendispuut P.C.S.A. Incognito</h1>
+          </div>
         </section>
-    </div>
-    <?php endif; ?>
+    <?php endif ?>
 
-    <section>
-        <?php print render($page['bottom']); ?>
-    </section>
+    <div id="main-wrapper" class="container"><div id="main" class="row clearfix">
+	
+    <?php if ($breadcrumb): ?><div id="breadcrumb" class="col-md-12">
+	 <?php print $breadcrumb; ?>
+	</div><?php endif; ?>
+	
+	<?php if ($messages): ?><div class="col-md-12">
+    <?php print $messages; ?>
+	</div><?php endif; ?>
+	
+      <div id="content" class="column <?php echo $contentclass; ?>"><div class="section">
+        <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+        <?php print render($page['content']); ?>
+        <?php print $feed_icons; ?>
+      </div></div> <!-- /.section, /#content -->
 
-    <section id="footer">
-        Copyright &copy; 1991-<?php echo date("Y"); ?> Heerendispuut P.C.S.A. Incognito. Alle rechten voorbehouden.
-        <?php print render($page['footer']); ?>
-    </section>
+      <?php if ($page['sidebar_first']): ?>
+        <div id="sidebar-first" class="column sidebar col-sm-3 <?php echo $firstsidebarpush; ?>"><div class="section">
+          <?php print render($page['sidebar_first']); ?>
+        </div></div> <!-- /.section, /#sidebar-first -->
+      <?php endif; ?>
 
+      <?php if ($page['sidebar_second']): ?>
+        <div id="sidebar-second" class="column sidebar col-sm-3"><div class="section">
+          <?php print render($page['sidebar_second']); ?>
+        </div></div> <!-- /.section, /#sidebar-second -->
+      <?php endif; ?>
 
+    </div></div> <!-- /#main, /#main-wrapper -->
 
-</div>
+    <div id="footer" class="container"><div class="row section">
+      <?php print render($page['footer']); ?>
+    </div></div> <!-- /.section, /#footer -->
+
+  </div></div> <!-- /#page, /#page-wrapper -->
