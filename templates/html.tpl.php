ce3c4a690af77087d428e9042d0678387b4c3dd7
Erik Steenman <eriksteenman@gmail.com>
2017-09-08 19:58:21 +0000

    Disable fixed height body.
    Fixes #58, but makes the scrollbar render over the topbar (which is a
    bit ugly).

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -52,7 +52,7 @@
   <?php print $styles; ?>
   <?php print $scripts; ?>
 </head>
-<body class="<?php print $classes; ?> noScroll" <?php print $attributes;?>>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
