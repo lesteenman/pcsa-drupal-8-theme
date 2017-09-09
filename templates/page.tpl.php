9be2f7a78b3a35a8de817fa877151df65aa7ef93
Erik Steenman <eriksteenman@gmail.com>
2017-09-09 09:25:46 +0000

    Various improvements in navbar alignment.
    Fixes #39

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -75,18 +75,18 @@
 
     <?php if ($main_menu || $secondary_menu): ?>
       <nav class="navbar navbar-default navbar-fixed-top">
+		    <?php if ($site_name): ?>
+		      <a class="navbar-brand" href="/"><?php print $site_name; ?></a>
+		    <?php endif; ?>
         <div class="container">
 
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
+				      <span class="sr-only">Toggle navigation</span>
+				      <span class="icon-bar"></span>
+				      <span class="icon-bar"></span>
+				      <span class="icon-bar"></span>
             </button>
-            <?php if ($site_name): ?>
-              <a class="navbar-brand" href="/"><?php print $site_name; ?></a>
-            <?php endif; ?>
           </div>
 
           <!-- display the navigation items, load them from the selected main_menu in drupal -->
